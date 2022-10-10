445552ff90db04941f69a5f88e743e187a988392
vitoriapinheiro <vps3@cin.ufpe.br>
2022-10-09 22:39:45 -0300

    setting up default configurations in the project

---
+++ SkinScanner/SkinScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkinScanner
+//
+//  Created by vivi on 09/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
