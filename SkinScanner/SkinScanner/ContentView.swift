f5d0eab39c4f24f65214662c12c576b4adfe0da1
vitoriapinheiro <vps3@cin.ufpe.br>
2022-10-14 09:43:26 -0300

    connected to the model

--- SkinScanner/SkinScanner/ContentView.swift
+++ SkinScanner/SkinScanner/ContentView.swift
@@ -10,6 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @AppStorage("showOnboarding") var showOnboarding: Bool = true
     
+    let model = MobileNetV2()
+    
+    private func performImageClassification(){
+        guard let img = UIImage(named: "cat"),
+        let ri = img.resizeTo(size: CGSize(width: 224, height: 224)),
+        let b = ri.toBuffer() else {
+            return
+        }
+        
+        let output = try? model.prediction(image: b)
+        
+        var res = ""
+        if let output = output {
+            res = output.classLabel
+        }
+        
+        print(res)
+    }
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -17,6 +36,12 @@ struct ContentView: View {
                     .imageScale(.large)
                     .foregroundColor(.accentColor)
                 Text("Home Page")
+                Button {
+                    self.performImageClassification()
+                } label: {
+                    Text("Iniciar")
+                }
+
             }
             .padding()
             .navigationTitle("Home")
